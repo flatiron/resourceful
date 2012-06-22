b0291a39090a2217aca9a2ba30c83cc06e1d991d
Marak Squires <marak.squires@gmail.com>
2012-06-21 23:05:12 -0700

    [examples] Expanded on parent-child example.

--- examples/relationship-parent-child.js
+++ examples/relationship-parent-child.js
@@ -5,25 +5,37 @@ var Category = resourceful.define('category');
 Category.parent('category');
 
 Category.create({
-  _id: 'hip-hop',
-}, function(err, hiphop){
-  Category.create({
-    _id: 'rap',
-  }, function(err, rap){
-    hiphop.createCategory({ _id: "a-tribe-called-quest", title: "Hello!" }, function(err, result){
-      hiphop.createCategory({ _id: "busta-rhymes", title: "Hello!" }, function(err, result){
-        rap.createCategory({ _id: "wu-tang", title: "Hello!" }, function(err, result){
-          rap.categories(function(err, result){
-            console.log(err, result);
-          });
-          hiphop.categories(function(err, result){
-            console.log(err, result);
-          });
-          Category.categories('hip-hop', function(err, result){
-            console.log('hip-hop', result)
-          });
-          Category.categories('rap', function(err, result){
-            console.log('rap', result)
+  _id: 'music',
+}, function(err, music){
+  music.createCategory({
+    _id: 'hip-hop',
+  }, function(err, hiphop){
+    music.createCategory({
+      _id: 'rap',
+    }, function(err, rap){
+      hiphop.createCategory({ _id: "a-tribe-called-quest", title: "Hello!" }, function(err, result){
+        hiphop.createCategory({ _id: "busta-rhymes", title: "Hello!" }, function(err, busta){
+          rap.createCategory({ _id: "wu-tang", title: "Hello!" }, function(err, wutang){
+            wutang.createCategory({ _id: "Enter the 36 Chambers", title: "Hello!" }, function(err, result){
+              music.categories(function(err, result){
+                console.log('music', err, result);
+              });
+              rap.categories(function(err, result){
+                console.log('rap', err, result);
+              });
+              hiphop.categories(function(err, result){
+                console.log('hiphop', err, result);
+              });
+              wutang.categories(function(err, result){
+                console.log('wutang', err, result);
+              });
+              Category.categories('music', function(err, result){
+                console.log('music', result)
+              });
+              Category.categories('music/rap', function(err, result){
+                console.log('music/rap', result)
+              });
+            });
           });
         });
       });
