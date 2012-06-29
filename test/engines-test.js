e0eee436b1ce9ca66f73794d3b3d4d436e7ce98e
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2012-06-29 20:52:44 +0530

    [test] Added some assertions in all() and find()

--- test/engines-test.js
+++ test/engines-test.js
@@ -35,6 +35,9 @@ engines.forEach(function (e) {
           assert.isNull(err);
           assert.isArray(obj);
           assert.equal(obj.length, 3);
+          assert.equal(obj[0]._id, 'bob');
+          assert.equal(obj[1]._id, 'mat');
+          assert.equal(obj[2]._id, 'tim');
         }
       }
     }
@@ -229,6 +232,10 @@ engines.forEach(function (e) {
             assert.equal(obj[0].age, 35);
             assert.equal(obj[0].hair, 'black');
             assert.equal(obj[0].resource, 'Author');
+            assert.equal(obj[1]._id, 'mat');
+            assert.equal(obj[1].age, 29);
+            assert.equal(obj[1].hair, 'black');
+            assert.equal(obj[1].resource, 'Author');
           },
           "should not be a new record": function (err, obj) {
             assert.isNull(err);
