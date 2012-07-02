555e8a4746a1fb5894e7db9df7b315ecff59f6d8
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2012-07-03 01:34:51 +0530

    [test] Added tests for listing and creating children from the Parent resource

--- test/relationship-test.js
+++ test/relationship-test.js
@@ -52,22 +52,48 @@ engines.forEach(function (e) {
               assert.equal(obj.repository_ids[0], 'bullet');
               assert.equal(obj.repository_ids[1], 'octonode');
             },
-            "on which '.repositories' is called": {
+            "when 'Parent.children()' is used": {
               topic: function (obj) {
-                obj.repositories(this.callback);
+                resources[e].User.repositories('pavan', this.callback);
               },
-              "should return them": function (obj) {
+              "should return them": function (err, obj) {
+                assert.isNull(err);
                 assert.lengthOf(obj, 2);
                 assert.equal(obj[0]._id, 'bullet');
                 assert.equal(obj[0].name, 'bullet');
                 assert.equal(obj[1]._id, 'octonode');
                 assert.equal(obj[0].name, 'octonode');
               },
-              "should be of proper resource type": function (obj) {
+              "should be of proper resource type": function (err, obj) {
+                assert.isNull(err);
                 assert.equal(obj[0].resource, 'Repository');
                 assert.equal(obj[1].resource, 'Repository');
               },
-              "should have the user_id set correctly": function (obj) {
+              "should have the user_id set correctly": function (err, obj) {
+                assert.isNull(err);
+                assert.equal(obj[0].user_id, 'pavan');
+                assert.equal(obj[1].user_id, 'pavan');
+              }
+            },
+            "when 'Parent.prototype.children()' is used": {
+              topic: function (obj) {
+                obj.repositories(this.callback);
+              },
+              "should return them": function (err, obj) {
+                assert.isNull(err);
+                assert.lengthOf(obj, 2);
+                assert.equal(obj[0]._id, 'bullet');
+                assert.equal(obj[0].name, 'bullet');
+                assert.equal(obj[1]._id, 'octonode');
+                assert.equal(obj[0].name, 'octonode');
+              },
+              "should be of proper resource type": function (err, obj) {
+                assert.isNull(err);
+                assert.equal(obj[0].resource, 'Repository');
+                assert.equal(obj[1].resource, 'Repository');
+              },
+              "should have the user_id set correctly": function (err, obj) {
+                assert.isNull(err);
                 assert.equal(obj[0].user_id, 'pavan');
                 assert.equal(obj[1].user_id, 'pavan');
               }
@@ -92,7 +118,7 @@ engines.forEach(function (e) {
           assert.equal(obj.resource, 'User');
           assert.lengthOf(obj.repository_ids, 2);
         },
-        "on which '.createRepository' is called": {
+        "with 'Parent.prototype.createChild()' being used": {
           "when successful": {
             topic: function (obj) {
               obj.createRepository({ _id: 'issues', name: 'issues'}, this.callback);
@@ -132,6 +158,77 @@ engines.forEach(function (e) {
                 assert.equal(obj._id, 'user/christian/issues');
                 assert.equal(obj.name, 'issues');
                 assert.equal(obj.user_id, 'christian');
+              }
+            }
+          },
+          "when unsuccessful using same _id": {
+            topic: function (obj) {
+              obj.createRepository({ _id: 'repository-1', name: 'reposit' }, this.callback);
+            },
+            "should respond with error": function (err, obj) {
+              assert.equal(err.error, 'conflict');
+              assert.isUndefined(obj);
+            }
+          }
+        }
+      }
+    }
+  }).addBatch({
+    "In database 'test'": {
+      topic: function () {
+        return null;
+      },
+      "an user named 'marak'": {
+        topic: function () {
+          resources[e].User.get('marak', this.callback);
+        },
+        "should exist": function (err, obj) {
+          assert.isNull(err);
+          assert.equal(obj._id, 'marak');
+          assert.equal(obj.name, 'marak');
+          assert.equal(obj.resource, 'User');
+          assert.lengthOf(obj.repository_ids, 2);
+        },
+        "with 'Parent.createChild()' being used": {
+          "when successful": {
+            topic: function (obj) {
+              resources[e].User.createRepository('marak', { _id: 'support', name: 'support'}, this.callback);
+            },
+            "should return the newly created object": function (err, obj) {
+              assert.isNull(err);
+              assert.equal(obj._id, 'user/marak/support');
+              assert.equal(obj.name, 'support');
+              assert.equal(obj.resource, 'Repository');
+            },
+            "should set the user_id correctly": function (err, obj) {
+              assert.isNull(err);
+              assert.equal(obj.user_id, 'marak');
+            },
+            "and reloading parent object": {
+              topic: function (child, parent) {
+                parent.reload(this.callback);
+              },
+              "should be successful": function (err, obj) {
+                assert.isNull(err);
+                assert.equal(obj._id, 'marak');
+                assert.equal(obj.name, 'marak');
+                assert.equal(obj.resource, 'User');
+              },
+              "should contain the new child object in the array": function (err, obj) {
+                assert.isNull(err);
+                assert.lengthOf(obj.repository_ids, 3);
+                assert.include(obj.repository_ids, 'support');
+              }
+            },
+            "should create the record in the db": {
+              topic: function () {
+                resources[e].Repository.get('user/marak/support', this.callback);
+              },
+              "should respond with the right object": function (err, obj) {
+                assert.isNull(err);
+                assert.equal(obj._id, 'user/marak/support');
+                assert.equal(obj.name, 'support');
+                assert.equal(obj.user_id, 'marak');
               }
             }
           },
