015db6a46ab23fedbe1b441d7449ab8ded1b73c9
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2012-08-03 19:06:13 +0530

    [test] Fix couchdb load error caused js ref

--- test/engines/memory.js
+++ test/engines/memory.js
@@ -5,6 +5,7 @@ engine.options = { uri: 'test' };
 
 engine.load = function (resourceful, data, callback) {
   data = data.map(function (r) {
+    r = JSON.parse(JSON.stringify(r));
     r.id = r._id;
     delete r._id;
     return r;
