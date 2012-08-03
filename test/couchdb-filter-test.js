2bb963deb50f5990248137ea52a194261684fd8e
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2012-08-03 16:48:38 +0530

    [fix] Fix bugs in views and filters

--- test/couchdb-filter-test.js
+++ test/couchdb-filter-test.js
@@ -8,101 +8,92 @@ var Article;
 
 resourceful.env = 'test';
 
-vows.describe('resourceful/resource/view').addVows({
+var couch = require('./engines/couchdb');
+
+vows.describe('resourceful/resource/view').addBatch({
   "A database containing articles and other resources": {
     topic: function () {
-      resourceful.use('couchdb', 'couchdb://localhost:5984/test');
-      var promise = new(events.EventEmitter);
-      var db = new(cradle.Connection)().database('test');
-      db.destroy(function () {
-        db.create(function () {
-          db.save([
-            { _id: 'article/1', resource: 'Article', title: 'The Great Gatsby', published: true,  author: 'fitzgerald', tags: ['classic'] },
-            { _id: 'article/2', resource: 'Article', title: 'Finding vim',      published: false, author: 'cloudhead',  tags: ['hacking', 'vi'] },
-            { _id: 'article/3', resource: 'Article', title: 'On Writing',       published: true,  author: 'cloudhead',  tags: ['writing'] },
-            { _id: 'article/4', resource: 'Article', title: 'vi Zen',           published: false, author: 'cloudhead',  tags: ['vi', 'zen'] },
-            { _id: 'article/5', resource: 'Article', title: 'Channeling force', published: true,  author: 'yoda',       tags: ['force', 'zen'] },
-          ], function () {
-            promise.emit('success');
-          });
-        });
-      })
-      return promise;
+      resourceful.use(couch.name, couch.options);
+      couch.load(resourceful, [
+        { _id: 'article/1', resource: 'Article', title: 'The Great Gatsby', published: true,  author: 'fitzgerald', tags: ['classic'] },
+        { _id: 'article/2', resource: 'Article', title: 'Finding vim',      published: false, author: 'cloudhead',  tags: ['hacking', 'vi'] },
+        { _id: 'article/3', resource: 'Article', title: 'On Writing',       published: true,  author: 'cloudhead',  tags: ['writing'] },
+        { _id: 'article/4', resource: 'Article', title: 'vi Zen',           published: false, author: 'cloudhead',  tags: ['vi', 'zen'] },
+        { _id: 'article/5', resource: 'Article', title: 'Channeling force', published: true,  author: 'yoda',       tags: ['force', 'zen'] },
+      ], this.callback);
     },
-    "is created": function () {}
-  }
-}).addVows({
-  "A Resource definition with filters": {
-    topic: function () {
-      Article = resourceful.define('Article', function () {
-        this.use('couchdb', 'couchdb://127.0.0.1:5984/test');
-        this.property('author');
-        this.property('title');
-        this.property('published', Boolean);
+    "is created": function () {},
+    "A Resource definition with filters": {
+      topic: function () {
+        Article = resourceful.define('Article', function () {
+          this.property('author');
+          this.property('title');
+          this.property('published', Boolean);
 
-        this.filter('all', {});
-        this.filter('published', { published: true });
-        this.filter('by', function (author) { return { author: author } });
-      })
+          this.filter('all', {});
+          this.filter('published', { published: true });
+          this.filter('by', function (author) { return { author: author } });
+        })
 
-      Article.register();
-      return Article;
-    },
-    "should respond to the filters": function (R) {
-      assert.isFunction(R.published);
-      assert.isFunction(R.all);
-      assert.isFunction(R.by);
-    },
-    "can be used to query the database:": {
-      "<published>": {
-        topic: function (Article) {
-          this.Article = Article;
-          Article.published(this.callback);
-        },
-        "should return an array of all published Articles": function (e, res) {
-          var that = this;
-          assert.isArray(res);
-          assert.equal(res.length,3);
-          res.forEach(function (d) {
-            assert.isObject(d);
-            assert.instanceOf(d,that.Article);
-            assert.equal(d.constructor,that.Article);
-            assert.equal(d.resource,'Article');
-            assert.ok(d.published);
-          });
-        }
+        Article.register();
+        return Article;
       },
-      "<all>": {
-        topic: function (Article) {
-          Article.all(this.callback);
-        },
-        "should return an array of all Article records": function (e, res) {
-          assert.isArray(res);
-          assert.equal(res.length,5);
-        }
+      "should respond to the filters": function (R) {
+        assert.isFunction(R.published);
+        assert.isFunction(R.all);
+        assert.isFunction(R.by);
       },
-      "<by> 'cloudhead'": {
-        topic: function (Article) {
-          Article.by('cloudhead', this.callback);
+      "can be used to query the database:": {
+        "<published>": {
+          topic: function (Article) {
+            this.Article = Article;
+            Article.published(this.callback);
+          },
+          "should return an array of all published Articles": function (e, res) {
+            var that = this;
+            assert.isArray(res);
+            assert.equal(res.length,3);
+            res.forEach(function (d) {
+              assert.isObject(d);
+              assert.instanceOf(d,that.Article);
+              assert.equal(d.constructor,that.Article);
+              assert.equal(d.resource,'Article');
+              assert.ok(d.published);
+            });
+          }
         },
-        "should return an array of Article records by 'cloudhead'": function (e, res) {
-          assert.isArray(res);
-          assert.equal(res.length,3);
-          res.forEach(function (d) {
-            assert.isObject(d);
-            assert.equal(d.resource,'Article');
-            assert.equal(d.author,'cloudhead');
-          });
-        }
-      },
-      "<by> 'yoda'": {
-        topic: function (Article) {
-          Article.by('yoda', this.callback);
+        "<all>": {
+          topic: function (Article) {
+            Article.all(this.callback);
+          },
+          "should return an array of all Article records": function (e, res) {
+            assert.isArray(res);
+            assert.equal(res.length,5);
+          }
         },
-        "should return an array of Article records by 'yoda'": function (e, res) {
-          assert.isArray(res);
-          assert.equal(res.length,1);
-          assert.equal(res[0].author,'yoda');
+        "<by> 'cloudhead'": {
+          topic: function (Article) {
+            Article.by('cloudhead', this.callback);
+          },
+          "should return an array of Article records by 'cloudhead'": function (e, res) {
+            assert.isArray(res);
+            assert.equal(res.length,3);
+            res.forEach(function (d) {
+              assert.isObject(d);
+              assert.equal(d.resource,'Article');
+              assert.equal(d.author,'cloudhead');
+            });
+          }
+        },
+        "<by> 'yoda'": {
+          topic: function (Article) {
+            Article.by('yoda', this.callback);
+          },
+          "should return an array of Article records by 'yoda'": function (e, res) {
+            assert.isArray(res);
+            assert.equal(res.length,1);
+            assert.equal(res[0].author,'yoda');
+          }
         }
       }
     }
@@ -111,7 +102,6 @@ vows.describe('resourceful/resource/view').addVows({
   "A second Resource definition with filters": {
     topic: function () {
       return resourceful.define('Person', function () {
-        this.use('couchdb', 'couchdb://127.0.0.1:5984/test');
         this.property('name');
         this.property('position');
         this.property('age', Number);
