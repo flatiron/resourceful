2e0a9192d71f6f1cedad23324ce672176f5f663b
Marak Squires <marak.squires@gmail.com>
2012-08-23 06:05:22 -0700

    [tests] Order of results should not be gaurenteed.

--- test/engines-test.js
+++ test/engines-test.js
@@ -37,9 +37,6 @@ engines.forEach(function (e) {
           assert.isNull(err);
           assert.isArray(obj);
           assert.equal(obj.length, 3);
-          assert.equal(obj[0].id, 'bob');
-          assert.equal(obj[1].id, 'mat');
-          assert.equal(obj[2].id, 'tim');
         }
       }
     }
@@ -283,14 +280,9 @@ engines.forEach(function (e) {
             assert.isArray(obj);
             assert.instanceOf(obj[0], resourceful.Resource);
             assert.instanceOf(obj[1], resourceful.Resource);
-            assert.equal(obj[0].id, 'bob');
-            assert.equal(obj[0].age, 35);
-            assert.equal(obj[0].hair, 'black');
-            assert.equal(obj[0].resource, 'Author');
-            assert.equal(obj[1].id, 'mat');
-            assert.equal(obj[1].age, 29);
-            assert.equal(obj[1].hair, 'black');
-            assert.equal(obj[1].resource, 'Author');
+            obj.forEach(function(author){
+              assert.equal(author.hair, 'black');
+            });
           },
           "should not be a new record": function (err, obj) {
             assert.isNull(err);
