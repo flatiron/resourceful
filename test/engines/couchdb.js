12e14a69deef32287c367dc8e4424d23f299c222
Jarrett Cruger <jcrugzz@gmail.com>
2014-07-12 15:05:02 -0400

    [test fix] allow auth to be passed in from environment

--- test/engines/couchdb.js
+++ test/engines/couchdb.js
@@ -3,7 +3,7 @@ var cradle = require('cradle');
 var engine = exports;
 
 engine.name = 'couchdb';
-engine.options = { database: 'test' };
+engine.options = { database: 'test', auth: process.env.COUCH_AUTH };
 
 engine.load = function (resourceful, data, callback) {
   var db = new(cradle.Connection)(engine.options).database(engine.options.database);
